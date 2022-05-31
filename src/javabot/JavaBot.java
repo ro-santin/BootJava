b50b68def7428d0725f71d9411d304dc3dd644c7
Rogerio Adriano Santin <ro_santin_pb@hotmail.com>
2022-05-31 17:58:23 -0400

    Inicio
    Por enquanto movimenta, mouse e abre CMD

---
+++ src/javabot/JavaBot.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javabot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class JavaBot {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.awt.AWTException
+     */
+    public static void main(String[] args) throws AWTException {
+        // TODO code application logic here
+    Robot robo = new Robot();
+    
+    robo.setAutoDelay(500);
+    
+    robo.delay(2000);
+    
+         
+    
+    
+    robo.keyPress(KeyEvent.VK_WINDOWS);
+    robo.keyPress(KeyEvent.VK_R);
+    
+    robo.keyRelease(KeyEvent.VK_R);
+    robo.keyRelease(KeyEvent.VK_WINDOWS);
+    
+    robo.setAutoDelay(50);
+     RoboSoletrando roboSoletrando = new RoboSoletrando(robo);
+    roboSoletrando.escrever("CMD");
+    
+    robo.keyPress(KeyEvent.VK_ENTER);
+    robo.keyRelease(KeyEvent.VK_ENTER); 
+    
+     robo.setAutoDelay(50);
+     
+   
+    
+//    robo.setAutoDelay(300);
+//    
+//    robo.mouseMove(1080, 480);
+//    robo.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//    robo.mouseMove(500, 480);
+//    robo.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//    
+//    robo.keyPress(KeyEvent.VK_DELETE);
+//    robo.keyRelease(KeyEvent.VK_DELETE);
+//    
+     robo.setAutoDelay(50);
+     RoboSoletrando roboSoletrando2 = new RoboSoletrando(robo);
+     roboSoletrando.escrever("bora apagar tudo que tem nesse pc");
+    
+    
+    }
+    
+}
